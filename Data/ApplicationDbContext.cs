571d26fbca54246170e9cbd032b7be106ccf3d34
JuanCruzManzo <juancruzmanzo9@gmail.com>
2026-07-05 19:50:42 -0300

    Configure Entity Framework y ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace sgvf_api.Data;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
